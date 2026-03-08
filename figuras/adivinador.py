29a25ac1a380fc2cb8993515172f9d4bcc679234
DMau1420 <shootingdarcklight@gmail.com>
2026-03-07 19:26:03 -0600

    primer version (espero que ultima tambien xD) del adivinador de figuras geometricas

---
+++ figuras/adivinador.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+def cargador(archivo):
+    try:
+        with open(archivo, "r", encoding='utf-8') as f:
+            return json.load(f)
+    except FileExistsError:
+        print("El archivo no existe")
+        return
+    except json.JSONDecodeError:
+        print("El archivo no es un json")
+        return
+
+def main():
+    archivo = cargador("figuras.json")
+    if not archivo:
+        return
+    
+    preguntas = archivo["preguntas"]
+    grafo = archivo["grafo"]
+
+    nodo = "P0"
+
+
+    print("===== Adivinador =====")
+    print("Responde las preguntas para adivinar tu pregunta \n")
+
+    while nodo in preguntas:
+        txt_pregunta = preguntas[nodo]
+        opciones = grafo[nodo]
+
+        print(f"=> {txt_pregunta}")
+        teclas_validas = list(opciones.keys())
+        print(f"opciones validas {teclas_validas}")
+
+        respuesta = input("R => ").strip().upper()
+
+        if respuesta in opciones:
+            nodo = opciones[respuesta]
+        else:
+            print(f"{respuesta} no es una opcion valida. Intenta otra vez \n")
+    print(f"\n {'='*44}")
+    print(f"Tu figura es un {nodo}")
+    print(f"\n {'='*44}")
+
+main()
